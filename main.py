0a32176e281b016634900a304f257a956a29e28a
NWSOFT <z-c-ge@outlook.com>
2021-06-27 07:13:14 +0800

    Added red-yellow light

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-
-basic.show_leds("""
-. . . . .
-. . . . .
+RED = images.create_image("""
+. # # # .
+. # # # .
 . . . . .
 . . . . .
 . . . . .
 """)
+
+RED_YELLOW = images.create_image("""
+. # # # .
+. # # # .
+. # # # .
+. . . . .
+. . . . .
+""")
+
+def on_forever():
+    RED.show_image(0)
+    basic.pause(15000)
+    RED_YELLOW.show_image(0)
+    basic.pause(3000)
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
-basic.showLeds(`
-. . . . .
-. . . . .
+let RED = images.createImage(`
+. # # # .
+. # # # .
 . . . . .
 . . . . .
 . . . . .
 `)
+let RED_YELLOW = images.createImage(`
+. # # # .
+. # # # .
+. # # # .
+. . . . .
+. . . . .
+`)
+basic.forever(function on_forever() {
+    RED.showImage(0)
+    basic.pause(15000)
+    RED_YELLOW.showImage(0)
+    basic.pause(3000)
+})
