d99f4299ddef62a3f894758a9e6a0fed87c7a956
NWSOFT <z-c-ge@outlook.com>
2021-06-27 08:38:32 +0800

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -13,10 +13,35 @@ RED_YELLOW = images.create_image("""
 . . . . .
 . . . . .
 """)
+YELLOW = images.create_image("""
+. . . . .
+. # # # .
+. # # # .
+. . . . .
+. . . . .
+""")
+GREEN = images.create_image("""
+. . . . .
+. . . . .
+. # # # .
+. # # # .
+. . . . .
+""")
 
 def on_forever():
     RED.show_image(0)
     basic.pause(15000)
     RED_YELLOW.show_image(0)
     basic.pause(3000)
+    GREEN.show_image(0)
+    basic.pause(15000)
+    YELLOW.show_image(0)
+    basic.pause(3000)
+
 basic.forever(on_forever)
+
+def on_button_pressed_a():
+    basic.clear_screen()
+    basic.show_string("Program exit.")
+    control.reset()
+input.on_button_pressed(Button.A, on_button_pressed_a)
--- main.ts
+++ main.ts
@@ -12,9 +12,32 @@ let RED_YELLOW = images.createImage(`
 . . . . .
 . . . . .
 `)
+let YELLOW = images.createImage(`
+. . . . .
+. # # # .
+. # # # .
+. . . . .
+. . . . .
+`)
+let GREEN = images.createImage(`
+. . . . .
+. . . . .
+. # # # .
+. # # # .
+. . . . .
+`)
 basic.forever(function on_forever() {
     RED.showImage(0)
     basic.pause(15000)
     RED_YELLOW.showImage(0)
     basic.pause(3000)
+    GREEN.showImage(0)
+    basic.pause(15000)
+    YELLOW.showImage(0)
+    basic.pause(3000)
+})
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    basic.clearScreen()
+    basic.showString("Program exit.")
+    control.reset()
 })
